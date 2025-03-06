da8bbaf3bf0793d2d7ba9f51424ceeb6e496728a
mahdishahy <mahdishahipro@gmail.com>
2025-03-06 13:51:25 +0330

    add: add a simple route. #2

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 
+app.get('/', (req, res) => {
+    return res.status(200).json({ message: "Welcome to noghte's API" })
+})
+
 module.exports = app
