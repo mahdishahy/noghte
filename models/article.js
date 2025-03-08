a8c7c9597e716718c0d4399ca24bc046c4c4ca7f
Hamed Akbarzadeh <h.akbarzadh@gmail.com>
2025-03-08 12:07:07 +0330

    add : add article model and add slugify package . #7

---
+++ models/article.js
@@ -0,0 +1,59 @@
+const { Schema, default: mongoose } = require("mongoose");
+const slugify = require("slugify")
+
+const schema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 1,
+    },
+    content: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 10
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    image_url: {
+        type: String,
+        default: null
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    collaborators: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ],
+    tags:
+        [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Tag",
+                default: null
+            }
+        ],
+    status: {
+        type: String,
+        enum: ["DRAFT", "PUBLISHED", "PENDING"],
+        default: "PENDING"
+    },
+}, { timestamps: true });
+
+schema.pre("save", function (next) {
+    this.slug = slugify(this.title, { lower: true, replacement: "-" })
+    next();
+});
+
+const model = mongoose.model("Article", schema);
+
+module.exports = model;
