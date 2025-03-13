80ebc1a5f949d22226c8d2bbba4830de67fd1fd6
mahdishahy <mahdishahipro@gmail.com>
2025-03-13 22:31:41 +0330

    add: add authentication middleware. #24

---
+++ middlewares/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+const userModel = require('./../models/user')
+
+module.exports = async (req, res, next) => {
+    const authHeader = req.header('Authorization')?.split(' ')
+    if ( authHeader?.length !== 2 ) {
+        return res.status(403).json({ message: 'لطفا ثبت نام کنید' })
+    }
+
+    const token = authHeader[1]
+    try {
+        const payloadData = jwt.verify(token, process.env.JWT_SECRET)
+        const user = await userModel.findById(payloadData.id).lean()
+        req.user = user
+        next()
+    } catch ( error ) {
+        return res.status(500).json({ message: 'خطا در سرور'})
+    }
+}
