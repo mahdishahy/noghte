d944591de7d8e90e5ad94146711c56e364bf7127
mahdishahy <mahdishahipro@gmail.com>
2025-03-13 22:35:07 +0330

    add: add ban user controller route. #23

---
+++ controllers/v1/ban.js
@@ -0,0 +1,39 @@
+const userModel = require('./../../models/user')
+const mongoose = require('mongoose')
+
+exports.suspend = async (req, res) => {
+    try {
+        const { id } = req.params
+        const { reason } = req.body
+
+        // id validation
+        const isValidId = mongoose.Types.ObjectId.isValid(id)
+        if ( !isValidId ) {
+            return res.status(409).json({ message: 'شناسه کاربر نامعتبر است' })
+        }
+
+        // reason validation
+        if ( reason === '' || !reason ) {
+            return res.status(409).json({ message: 'دلیل تعلیق کاربر الزامی است' })
+        }
+
+        // get main user with id
+        const mainUser = await userModel.findOne({ _id: id })
+        if ( !mainUser ) {
+            return res.status(404).send({ message: 'کاربر مورد نظر یافت نشد' })
+        }
+
+        // check id the user is already suspend
+        if ( mainUser.is_suspended ) {
+            return res.status(409).json({ message: 'کاربر از قبل تعلیق شده' })
+        }
+
+        const suspend = await userModel.findByIdAndUpdate(id, {
+            is_suspended: true,
+            suspension_reason: reason
+        })
+        return res.status(201).json({ messgae: 'کاربر تعلیق شد' })
+    } catch ( error ) {
+        return res.status(500).json({ message: "خطا در سرور" })
+    }
+}
