80602e208175010c590788e0958085c46a1573ec
Hamed Akbarzadeh <h.akbarzadh@gmail.com>
2025-03-28 17:34:33 +0330

    update : add route for userController and fix bug in userController. #32

--- routes/v1/user.js
+++ routes/v1/user.js
@@ -3,9 +3,16 @@ const express = require('express')
 const banController = require('./../../controllers/v1/ban')
 const authMiddleware = require('./../../middlewares/auth')
 const isAdminMiddleware = require('./../../middlewares/isAdmin')
+const userController = require('./../../controllers/v1/user')
 
 const router = express.Router()
 
 router.route('/suspend/:id').put(authMiddleware, isAdminMiddleware, banController.suspend)
+
+router.get('/', authMiddleware, isAdminMiddleware, userController.getAll)
+router.get('/:id', authMiddleware, isAdminMiddleware, userController.getById)
+router.delete('/:id', authMiddleware, isAdminMiddleware, userController.remove)
+router.put('/:id', authMiddleware, isAdminMiddleware, userController.update)
+router.post('/change-role/:id', authMiddleware, isAdminMiddleware, userController.changeRole)
 
 module.exports = router
