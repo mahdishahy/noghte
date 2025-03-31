cb7276bde56f18a0b84fac018b1e0bffbbcb7049
Hamed Akbarzadeh <h.akbarzadh@gmail.com>
2025-04-01 01:04:57 +0330

    add : add category section.

---
+++ models/category.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const { generateSlug } = require("../middlewares/article");
+
+const schema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 1,
+    }
+}, { timestamps: true });
+
+
+const model = mongoose.model("Category", schema);
+
+module.exports = model;
