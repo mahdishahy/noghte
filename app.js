4791c5c9a516284807049a67c0b926544b847348
mahdishahy <mahdishahipro@gmail.com>
2025-04-07 20:53:20 +0330

    add: add followers and followeing in the get user by username. #41

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet')
 const cors = require('cors')
+const publicRoutes = require('./routes/v1/publicRoutes')
 const authRouter = require('./routes/v1/auth')
 const usersRouter = require('./routes/v1/user')
 const articlesRouter = require('./routes/v1/article')
@@ -17,7 +18,7 @@ app.use(cors())
 app.get('/', (req, res) => {
     return res.status(200).json({ message: "Welcome to noghte's API" })
 })
-app.get(/^\/@([a-zA-Z0-9_]{5,15})$/, userController.getByUserName);
+app.use('/', publicRoutes)
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', usersRouter)
 app.use('/api/v1/articles', articlesRouter)
