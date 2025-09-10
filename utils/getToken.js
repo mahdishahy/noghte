43661cbda74a2214cdc314449d0b56dd2378e554
mahdishahy <mahdishahipro@gmail.com>
2025-09-10 16:52:37 +0330

    update: Change access token expire time to 1h

--- utils/getToken.js
+++ utils/getToken.js
@@ -4,7 +4,7 @@ exports.generateTokens = (user) => {
     const accessToken = jwt.sign({
         userId: user._id,
         email: user.email
-    }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15m' })
+    }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
 
     const refreshToken = jwt.sign({
         userId: user._id,
