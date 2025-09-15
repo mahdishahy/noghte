fb487c8b943205d9ccb21cbc954bb389b37da673
mahdishahy <mahdishahipro@gmail.com>
2025-09-15 09:54:00 +0330

    update: add expire time variable for tokens in env.

--- utils/getToken.js
+++ utils/getToken.js
@@ -4,12 +4,12 @@ exports.generateTokens = (user) => {
     const accessToken = jwt.sign({
         userId: user._id,
         email: user.email
-    }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+    }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: process.env.ACCESS_TOKEN_EXPIRE_TIME + 'm' })
 
     const refreshToken = jwt.sign({
         userId: user._id,
         email: user.email
-    }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '30d' })
+    }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: process.env.REFRESH_TOKEN_EXPIRE_TIME + 'd' })
 
     return { accessToken, refreshToken }
 }
